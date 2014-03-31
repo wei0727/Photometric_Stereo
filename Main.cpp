ae9d27e6d313bd9c434cbcf1a5ef9a4c19b337de
wei0727 <phonneog@gmail.com>
2014-03-31 21:09:32 +0800

    Final version
    basic surface reconstruct
    background depth = 0

--- Main.cpp
+++ Main.cpp
@@ -9,25 +9,28 @@ using namespace std ;
 using namespace cv ;
 
 void normalColorMap(IplImage *img[6], Mat lights, IplImage *nImg, vector<vector<Vec3d>> &nMap){
+	//Ux = y
+	// x = inverse(transpose(U)*U) * transpose(U) * y
+	//preompute inverse(transpose(U)*U) * transpose(U)
 	Mat sMat = (lights.t()*lights).inv() * lights.t() ;
 	for(int row=0; row<img[0]->height; row++){
 		for(int col=0; col<img[0]->width; col++){
 			double iArr[6] ;
 			bool nonZero = false ;
+			//get gray level from 6 images
 			for(int i=0; i<6; i++){
 				iArr[i] = cvGetReal2D(img[i], row, col) ;
-				if(iArr[i] > 0) nonZero = true ;
 			}
 			Mat iMat = Mat(6, 1, CV_64F, iArr) ;
 			Mat bMat = sMat * iMat ;
+			//(z, y, x) --> (b, g, r)
 			Vec3d c(bMat.at<double>(2, 0), -bMat.at<double>(1, 0), bMat.at<double>(0, 0)) ;
 			if(norm(c)!=0)
 				c = c / norm(c) ;
 			nMap[row][col] = Vec3d(c[2], c[1], c[0]) ;
+			//normalize to 0~255
 			c = (c+Vec3d(1, 1, 1))*255/2 ;
-			if(nonZero){
-				//cout << iMat << endl << bMat << endl ;
-			}
+			//set color to nImg
 			cvSet2D(nImg, row, col, c) ;
 		}
 	}
@@ -38,14 +41,15 @@ void surfaceReconstruct(string fileName, vector<vector<Vec3d>> &nMap){
 	const int c = nMap[0].size() ;
 	ofstream depthFile(fileName) ;
 	depthFile << 0.05 << endl ;
-	depthFile << r << endl ;
 	depthFile << c << endl ;
+	depthFile << r << endl ;
 	vector<vector<double>> heightField(r, vector<double>(c, 0.0)) ;
 	double integral_y=0, integral_x=0 ;
 	double dmax, dmin ;
 	dmax = dmin = 0 ;
 	//calculate depth
 	for(int row=1; row<r; row++){
+		//prevent divide by zero
 		if(nMap[row][0][2]!=0)
 			heightField[row][0] = heightField[row-1][0] + -nMap[row][0][1]/nMap[row][0][2] ;
 		else
@@ -70,16 +74,18 @@ void surfaceReconstruct(string fileName, vector<vector<Vec3d>> &nMap){
 			if(norm(nMap[row][col])!=0)
 				//normalize depth to 0~5
 				depthFile << (heightField[row][col]-dmin)*5/(dmax-dmin) << " " ;
+				//depthFile << heightField[row][col] << " " ;
 			else
 				depthFile << 0 << " " ;
 		}
 		depthFile << endl ;
 	}
 	depthFile.close() ;
+	//cout << "max min: " << dmax << "\t" << dmin << endl ;
 }
 
-void surfaceReconstruct_lravg(string fileName, vector<vector<Vec3d>> &nMap){
-		const int r = nMap.size() ;
+void surfaceReconstruct_avg(string fileName, vector<vector<Vec3d>> &nMap){
+	const int r = nMap.size() ;
 	const int c = nMap[0].size() ;
 	ofstream depthFile(fileName) ;
 	depthFile << 0.05 << endl ;
@@ -91,29 +97,29 @@ void surfaceReconstruct_lravg(string fileName, vector<vector<Vec3d>> &nMap){
 	double dmax, dmin ;
 	dmax = dmin = 0 ;
 	//calculate depth
+	//left top to right bottom
 	for(int row=1; row<r; row++){
 		if(nMap[row][0][2]!=0)
-			heightFieldR[row][0] = heightFieldR[row-1][0] + -nMap[row][0][1]/nMap[row][0][2] ;
+			heightFieldL[row][0] = heightFieldL[row-1][0] + -nMap[row][0][1]/nMap[row][0][2] ;
 		else
-			heightFieldR[row][0] = heightFieldR[row-1][0] ;
-		if(nMap[row][c-1][2]!=0)
-			heightFieldL[row][c-1] = heightFieldL[row-1][c-1] + -nMap[row][c-1][1]/nMap[row][c-1][2] ;
-		//dmax = std::max(dmax, heightFieldL[row][0]) ;
-		//dmin = std::min(dmin, heightFieldL[row][0]) ;
+			heightFieldL[row][0] = heightFieldL[row-1][0] ;
 	}
-	//left to right
 	for(int row=0; row<r; row++){
 		for(int col=1; col<c; col++){
 			if(nMap[row][col][2]!=0)
 				heightFieldL[row][col] = heightFieldL[row][col-1] + -nMap[row][col][0]/nMap[row][col][2] ;
 			else
 				heightFieldL[row][col] = heightFieldL[row][col-1] ;
-			//dmax = std::max(dmax, heightFieldL[row][col]) ;
-			//dmin = std::min(dmin, heightFieldL[row][col]) ;
 		}
 	}
-	//right to left
-	for(int row=0; row<r; row++){
+	//right bottom to left top
+	for(int row=r-2; row>=0; row--){
+		if(nMap[row][c-1][2]!=0)
+			heightFieldR[row][c-1] = heightFieldR[row+1][c-1] + -nMap[row][c-1][1]/nMap[row][c-1][2] ;
+		else
+			heightFieldR[row][c-1] = heightFieldR[row+1][c-1] ;
+	}
+	for(int row=r-1; row>=0; row--){
 		for(int col=c-2; col>=0; col--){
 			if(nMap[row][col][2]!=0)
 				heightFieldR[row][col] = heightFieldR[row][col+1] + -nMap[row][col][0]/nMap[row][col][2] ;
@@ -140,113 +146,50 @@ void surfaceReconstruct_lravg(string fileName, vector<vector<Vec3d>> &nMap){
 	depthFile.close() ;
 }
 
-int main(){
-	string path = "bunny/" ;
+void photometric(string path, double *lightArr){
 	IplImage *img[6] ;
- 	for(int i=0; i<6; i++){
-		cout << path + "pic" + (char)(i+49) + ".bmp" << endl ;
-		img[i] = cvLoadImage((path + "pic" + (char)(i+49) + ".bmp").c_str(), CV_LOAD_IMAGE_GRAYSCALE) ;
+	//load 6 image from given path(bunny or teapot)
+	for(int i=0; i<6; i++){
+		cout << path + "/pic" + (char)(i+49) + ".bmp" << endl ;
+		img[i] = cvLoadImage((path + "/pic" + (char)(i+49) + ".bmp").c_str(), CV_LOAD_IMAGE_GRAYSCALE) ;
 		//cvShowImage((path + (char)(i+49) + ".bmp").c_str(), img[i]) ;
+		//cout << img[i]->nChannels << endl ;
 	}
-	double lightArr[] = {238,235,2360,
+	Mat lights = Mat(6, 3, CV_64F, lightArr).clone() ;
+	//Normal color image
+	IplImage *nImg = cvCreateImage(cvSize(img[0]->width, img[0]->height), img[0]->depth, 3) ;
+	cvSetZero(nImg) ;
+	//Original Normals 
+	vector<vector<Vec3d>> nMap(img[0]->height, vector<Vec3d>(img[0]->width)) ;
+
+	//compute normal and set to nImg, nMap
+	normalColorMap(img, lights, nImg, nMap) ;
+	//compute depth value from given nMap and save to corresponding file
+	surfaceReconstruct(path+"_depth.txt", nMap) ;
+
+	//surfaceReconstruct_avg(path+"_depth_avg.txt", nMap) ;
+	cvSaveImage((path+"_normal.jpg").c_str(), nImg) ;
+	cout << path+"_normal.jpg  saved" << endl ;
+	cout << path+"_depth.txt  saved" << endl ;
+}
+
+int main(){
+	//light source of bunny
+	double lightArr_bunny[] = {238,235,2360,
 					298,65,2480,
 					-202,225,2240,
 					-252,115,2310,
 					18,45,2270,
 					-22,295,2230} ;
-	Mat lights = Mat(6, 3, CV_64F, lightArr).clone() ;
-	IplImage *nImg = cvCreateImage(cvSize(img[0]->width, img[0]->height), img[0]->depth, 3) ;
-	cvSetZero(nImg) ;
-	vector<vector<Vec3d>> nMap(img[0]->height, vector<Vec3d>(img[0]->width)) ;
-	normalColorMap(img, lights, nImg, nMap) ;
-
-	surfaceReconstruct("bunny_depth.txt", nMap) ;
-	surfaceReconstruct_lravg("bunny_depth_lravg.txt", nMap) ;
-
-	vector<vector<double>> heightField(nImg->height, vector<double>(nImg->width, 0.0)) ;
-	vector<vector<double>> pArr(nImg->height, vector<double>(nImg->width, 0.0)) ;
-	vector<vector<double>> qArr(nImg->height, vector<double>(nImg->width, 0.0)) ;
-	ofstream depthFile("depth_bunny.txt") ;
-	ofstream pFile("pFile.txt") ;
-	ofstream qFile("qFile.txt") ;
-	depthFile << 0.05 << endl ;
-	depthFile << 120 << endl ;
-	depthFile << 120 << endl ;
-	for(int row=1; row<nImg->height; row++){
-		if(nMap[row][0][2]!=0)
-			heightField[row][0] = heightField[row-1][0] + -nMap[row][0][1]/nMap[row][0][2] ;
-		else
-			heightField[row][0] = heightField[row-1][0] ;
-	}
-	for(int row=0; row<nImg->height; row++){
-		for(int col=1; col<nImg->width; col++){
-			if(nMap[row][col][2]!=0)
-				heightField[row][col] = heightField[row][col-1] + -nMap[row][col][0]/nMap[row][col][2] ;
-			else
-				heightField[row][col] = heightField[row][col-1] ;
-			
-		}
-	}
-	double max, min ;
-	double pmax, pmin, qmax, qmin ;
-	max = min = heightField[0][0] ;
-	pmax = pmin = qmax = qmin = 0 ;
-	int zeroCount = 0 ;
-	for(int row=0; row<nImg->height; row++){
-		for(int col=0; col<nImg->width; col++){
-			if(nMap[row][col][2]!=0){
-				pArr[row][col] = -nMap[row][col][0]/nMap[row][col][2] ;
-				qArr[row][col] = -nMap[row][col][1]/nMap[row][col][2] ;
-			}
-			else
-				zeroCount++ ;;
-			max = std::max(max, heightField[row][col]) ;
-			min = std::min(min, heightField[row][col]) ;
-			pmax = std::max(pmax, pArr[row][col]) ;
-			pmin = std::min(pmin, pArr[row][col]) ;
-			qmax = std::max(qmax, qArr[row][col]) ;
-			qmin = std::min(qmin, qArr[row][col]) ;
-			//depthFile << heightField[row][col]/10.0 << " " ;
-		}
-		//depthFile << endl ;
-	}
-	for(int row=0; row<nImg->height; row++){
-		for(int col=0; col<nImg->width; col++){
-			//if(heightField[row][col]!=0)
-			if(norm(nMap[row][col])!=0)
-				depthFile << (heightField[row][col]-min)*5/(max-min) << " " ;
-			else
-				depthFile << 0 << " " ;
-			
-			pFile << pArr[row][col] << "\t" ;
-			qFile << qArr[row][col] << "\t" ;
-		}
-		depthFile << endl ;
-		pFile << endl ;
-		qFile << endl ;
-	}
-	int count=0 ;
-	for(int row=1; row<nImg->height; row++){
-		for(int col=1; col<nImg->width; col++){
-			double dpdy = pArr[row][col]-pArr[row-1][col] ;
-			double dqdx = qArr[row][col]-qArr[row][col-1] ;
-			double tmp = dpdy-dqdx ;
-			tmp *= tmp ;
-			if(tmp > 10){
-				cout << tmp << endl ;
-				count++ ;
-			}
-		}
-	}
-	cout << "2nd derivative: " << count << endl ;
-	depthFile.close() ;
-	pFile.close() ;
-	qFile.close() ;
-	cout << "depth: " << max << "\t" << min << endl ;
-	cout << "p: " << pmax << "\t" << pmin << endl ;
-	cout << "q: " << qmax << "\t" << qmin << endl ;
-	//cvShowImage("nmap", nImg) ;
-	cvSaveImage("bunny_normal.jpg", nImg) ;
+	//light source of teapot
+	double lightArr_teapot[] = {0,0,150,
+								22,29,156,
+								-46,38,163,
+								-25,13,160,
+								12,10,168,
+								35,5,168} ;
+	photometric("bunny", lightArr_bunny) ;
+	photometric("teapot", lightArr_teapot) ;
 	cvWaitKey(0) ;
 	return 0 ;
 }
